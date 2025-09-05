e17dcb0a8f015308a48bcee39dcbf56451892155
ladlafreakhacker-wq <ladlafreakhacker@gmail.com>
2025-09-05 19:25:06 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const PORT = process.env.PORT || 3000;
 
 // Root check
 app.get("/", (req, res) => {
-  res.send("Server is working ✅ (no postback mode)");
+  res.send("Monetag-server is working ✅ (no postback mode)");
 });
 
-// List of Monetag direct links
+// List of Monetag direct links (replace with your IDs)
 const offers = {
-  1: "https://syndication.exoclick.com/splash.php?idzone= 9780832", // replace XXXXX
-  2: "https://syndication.exoclick.com/splash.php?idzone=9780834", // replace YYYYY
-  3: "https://syndication.exoclick.com/splash.php?idzone=9780829"  // replace ZZZZZ
+  1: "https://syndication.exoclick.com/splash.php?idzone=9780832",
+  2: "https://syndication.exoclick.com/splash.php?idzone=9780834",
+  3: "https://syndication.exoclick.com/splash.php?idzone=9780829"
 };
 
 // Click redirect
 app.get("/click", (req, res) => {
-  let offerid = req.query.offerid || 1; // default offer 1
-  let subid = req.query.subid || Date.now(); // track user or random ID
+  let offerid = req.query.offerid || 1; // default = offer 1
+  let subid = req.query.subid || Date.now(); // track user (or random ID)
 
   let baseUrl = offers[offerid];
   if (!baseUrl) return res.send("Offer not found ❌");
